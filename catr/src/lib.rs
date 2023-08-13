bb6e591ce0ab60417925603c36993413deb001a7
shafayetShafee <sshafee@isrt.ac.bd>
2023-08-13 22:09:31 +0600

    formatted the catr code

--- catr/src/lib.rs
+++ catr/src/lib.rs
@@ -16,21 +16,25 @@ type MyResult<T> = Result<T, Box<dyn Error>>;
 pub fn run(config: Config) -> MyResult<()> {
     for filename in config.files {
         match open(&filename) {
-	    Err(err) => eprintln!("Failed to open {}: {}", filename, err),
-	    Ok(file) => {
-		let mut line_count = 1;
-		for line in file.lines() {
-		    let line_content = line?;
-		    if config.number_nonblank_lines && line_content.is_empty() {
-	    	        println!();
-			continue;
-		    }
-		    let line_prefix = if config.number_lines || config.number_nonblank_lines {format!("{:>6}\t", line_count)} else { "".to_string() };  
-		    println!("{}{}", line_prefix, line_content);
-		    line_count += 1;
-		}
-	    },
-	}
+            Err(err) => eprintln!("Failed to open {}: {}", filename, err),
+            Ok(file) => {
+                let mut line_count = 1;
+                for line in file.lines() {
+                    let line_content = line?;
+                    if config.number_nonblank_lines && line_content.is_empty() {
+                        println!();
+                        continue;
+                    }
+                    let line_prefix = if config.number_lines || config.number_nonblank_lines {
+                        format!("{:>6}\t", line_count)
+                    } else {
+                        "".to_string()
+                    };
+                    println!("{}{}", line_prefix, line_content);
+                    line_count += 1;
+                }
+            }
+        }
     }
     Ok(())
 }
@@ -38,8 +42,8 @@ pub fn run(config: Config) -> MyResult<()> {
 fn open(filename: &str) -> MyResult<Box<dyn BufRead>> {
     match filename {
         "-" => Ok(Box::new(BufReader::new(io::stdin()))),
-	_ => Ok(Box::new(BufReader::new(File::open(filename)?))),
-    }    
+        _ => Ok(Box::new(BufReader::new(File::open(filename)?))),
+    }
 }
 
 pub fn get_args() -> MyResult<Config> {
@@ -49,31 +53,31 @@ pub fn get_args() -> MyResult<Config> {
         .about("Rust cat")
         .arg(
             Arg::with_name("files")
-		.value_name("FILE")
-		.help("Input file(s)")
-		.multiple(true)
-		.default_value("-")
-	)
-	.arg(
-	    Arg::with_name("number")
-		.short("n")
-		.long("number")
-		.help("Number lines")
-		.takes_value(false)
-		.conflicts_with("number_nonblank")
-	)
-	.arg(
-	    Arg::with_name("number_nonblank")
-		.short("b")
-		.long("number-nonblank")
-		.help("Number non-blank lines")
-		.takes_value(false)
-	)
-	.get_matches();
+                .value_name("FILE")
+                .help("Input file(s)")
+                .multiple(true)
+                .default_value("-"),
+        )
+        .arg(
+            Arg::with_name("number")
+                .short("n")
+                .long("number")
+                .help("Number lines")
+                .takes_value(false)
+                .conflicts_with("number_nonblank"),
+        )
+        .arg(
+            Arg::with_name("number_nonblank")
+                .short("b")
+                .long("number-nonblank")
+                .help("Number non-blank lines")
+                .takes_value(false),
+        )
+        .get_matches();
 
     Ok(Config {
-	files: matches.values_of_lossy("files").unwrap(),
-	number_lines: matches.is_present("number"),
-	number_nonblank_lines: matches.is_present("number_nonblank"),
+        files: matches.values_of_lossy("files").unwrap(),
+        number_lines: matches.is_present("number"),
+        number_nonblank_lines: matches.is_present("number_nonblank"),
     })
 }
--- catr/src/main.rs
+++ catr/src/main.rs
@@ -1,6 +1,6 @@
 fn main() {
     if let Err(e) = catr::get_args().and_then(catr::run) {
-    	eprintln!("{}", e);
-	std::process::exit(1);
+        eprintln!("{}", e);
+        std::process::exit(1);
     }
 }
